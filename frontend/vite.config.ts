e396bdddaff228fef956aaf411bee67cc84aeebb
King Igbozuruike <kingobiomaeze2020@gmail.com>
2025-06-15 09:52:01 +0000

    fixed routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -15,6 +16,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: ".",
+        },
+      ],
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
